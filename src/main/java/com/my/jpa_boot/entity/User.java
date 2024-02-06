9adaa97eea0149f0488f38563badc8de640e9ebd
jm90hong <jm90.hong@gmail.com>
2024-02-06 16:29:38 +0900

    1

--- src/main/java/com/my/jpa_boot/entity/User.java
+++ src/main/java/com/my/jpa_boot/entity/User.java
@@ -1,5 +1,46 @@
 package com.my.jpa_boot.entity;
 
-public class User {
+import java.time.LocalDateTime;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class User {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="user_idx")
+	private Long userIdx;
+	
+	
+	@Column(name="id")
+	private String id;
+	
+	
+	@Column(name="pw")
+	private String pw;
+	
+	
+	@Column(name="created_date")
+	private LocalDateTime createdDate;
+	
+	
+	
+	
 }
