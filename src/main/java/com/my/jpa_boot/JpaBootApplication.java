9bb350d38fa0bdc3f00c2500e9543c58341dc377
jm90hong <jm90.hong@gmail.com>
2024-02-06 16:03:49 +0900

    1

---
+++ src/main/java/com/my/jpa_boot/JpaBootApplication.java
@@ -0,0 +1,15 @@
+package com.my.jpa_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.my.jpa_boot.repository")
+public class JpaBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/my/jpa_boot/JpaBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.jpa_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
